b68003a7ffcebe4fb3182353c2655d74d02bcc41
Tatiana Streltsova <tstreltsova@gmail.com>
2021-05-19 18:31:58 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
     source  = "terraform-google-modules/network/google"
-    version = "2.5.0"
+    version = "~> 2.5"
 
     project_id   = var.project
     network_name = "gaurav-network"
@@ -11,28 +11,6 @@ module "vpc" {
             subnet_name   = "gaurav-subnet"
             subnet_ip     = "10.100.10.0/24"
             subnet_region = var.region
-        },
-        {
-            subnet_name           = "subnet-01"
-            subnet_ip             = "10.10.10.0/24"
-            subnet_region         = "us-west1"
-        },
-        {
-            subnet_name           = "subnet-02"
-            subnet_ip             = "10.10.20.0/24"
-            subnet_region         = "us-west1"
-            subnet_private_access = "true"
-            subnet_flow_logs      = "true"
-            description           = "This subnet has a description"
-        },
-        {
-            subnet_name               = "subnet-03"
-            subnet_ip                 = "10.10.30.0/24"
-            subnet_region             = "us-west1"
-            subnet_flow_logs          = "true"
-            subnet_flow_logs_interval = "INTERVAL_10_MIN"
-            subnet_flow_logs_sampling = 0.7
-            subnet_flow_logs_metadata = "INCLUDE_ALL_METADATA"
         }
     ]
 
